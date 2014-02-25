ff7d6d561ebb75a7c23d93e249ca2d5da39a46b4
davecrow <davecrow@gmail.com>
2014-02-24 23:43:54 -0700

    Add custom js for parallax scrolling

--- public/js/custom.js
+++ public/js/custom.js
@@ -39,6 +39,52 @@ $(function () {
 	});
 });
 
+// Parallax Scrolling Fanciness
+
+( function( $ ) {
+	
+	// Setup variables
+	$window = $(window);
+	$slide = $('.homeSlide');
+	$body = $('body');
+	
+    //FadeIn all sections   
+	$body.imagesLoaded( function() {
+		setTimeout(function() {
+		      
+		      // Resize sections
+		      adjustWindow();
+		      
+		      // Fade in sections
+			  $body.removeClass('loading').addClass('loaded');
+			  
+		}, 800);
+	});
+	
+	function adjustWindow(){
+		
+		// Init Skrollr
+		var s = skrollr.init();
+		
+		// Get window size
+	    winH = $window.height();
+	    
+	    // Keep minimum height 550
+	    if(winH <= 550) {
+			winH = 550;
+		} 
+	    
+	    // Resize our slides
+	    $slide.height(winH);
+	    
+	    // Refresh Skrollr after resizing our sections
+	    s.refresh($('.homeSlide'));
+	    
+	    
+	}
+		
+} )( jQuery );
+
 
 
 // $(function (){
