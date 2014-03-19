c2fd885c99e3191b06cc329d3c175b7473e51faa
Zach Pflederer <zpfled@gmail.com>
2014-03-19 12:02:46 -0500

    fix map on mobile

--- public/js/custom.js
+++ public/js/custom.js
@@ -310,6 +310,7 @@ $(function () {
 ( function( $ ) {
     // Init Skrollr
     var s = skrollr.init({
+    	forceheight: false,
         render: function(data) {
             //Debugging - Log the current scroll position.
             //console.log(data.curTop);
