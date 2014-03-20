5f705de2683ac495c4d5e0faef547a0d47f3705b
Zach Pflederer <zpfled@gmail.com>
2014-03-20 17:53:01 -0500

    broken welcome image on mobile

--- public/js/custom.js
+++ public/js/custom.js
@@ -330,7 +330,7 @@ $(function () {
 
 ( function( $ ) {
 	// Init Skrollr
-	// if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
+	if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
 		var s = skrollr.init({
 			render: function(data) {
 				// Debugging - Log the current scroll position.
@@ -339,49 +339,5 @@ $(function () {
 		});
 		// Refresh Skrollr after resizing our sections
 		s.refresh($('.homeSlide'));
-	// }
+	}
 } )( jQuery );
-
-// ( function( $ ) {
-	
-// 	// Setup variables
-// 	$window = $(window);
-// 	$slide = $('.homeSlide');
-// 	$body = $('body');
-	
-//  //    //FadeIn all sections   
-// 	// $body.imagesLoaded( function() {
-// 	// 	setTimeout(function() {
-		      
-// 	// 	      // Resize sections
-// 	// 	      adjustWindow();
-		      
-// 	// 	      // Fade in sections
-// 	// 		  $body.removeClass('loading').addClass('loaded');
-			  
-// 	// 	}, 800);
-// 	// });
-	
-// 	function adjustWindow(){
-		
-// 		// Init Skrollr
-// 		var s = skrollr.init();
-		
-// 		// Get window size
-// 	    winH = $window.height();
-	    
-// 	    // Keep minimum height 550
-// 	    if(winH <= 550) {
-// 			winH = 550;
-// 		} 
-	    
-// 	    // Resize our slides
-// 	    $slide.height(winH);
-	    
-// 	    // Refresh Skrollr after resizing our sections
-// 	    s.refresh($('.homeSlide'));
-	    
-	    
-// 	}
-		
-// } )( jQuery );
