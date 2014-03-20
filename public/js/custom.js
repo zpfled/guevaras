52c42d275eadd9412a55c9e81de3165fc66e5a48
Zach Pflederer <zpfled@gmail.com>
2014-03-20 17:34:38 -0500

    disable mobile skrollr

--- public/js/custom.js
+++ public/js/custom.js
@@ -329,7 +329,7 @@ $(function () {
 
 ( function( $ ) {
     // Init Skrollr
-    // if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
+    if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
     	var s = skrollr.init({
     	    render: function(data) {
     	        // Debugging - Log the current scroll position.
